60bcab8a34c602766d1f3a133acd59ea8dbdce25
= <=>
2024-06-03 21:42:24 +0800

    initial commit

---
+++ database/migrations/2024_06_03_132557_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+            $table->string('customer'); //1
+            $table->string('driver'); //2
+            $table->string('weigher'); //3
+            $table->string('gross');
+            $table->string('plate_number');
+            $table->string('weigh_in');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
