37feb9a3dd1c07f70fdbb38b091fc2b055f323ed
= <=>
2024-06-04 10:14:09 +0800

    datatable

--- routes/web.php
+++ routes/web.php
@@ -22,44 +22,8 @@ Route::get('/', function () {
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
-});
-
-Route::post('/dashboard', [LoginController::class, 'login'])->name('login');
-Route::get('/login', [LoginController::class, 'index']);
-
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/transactions', [TransactionController::class, 'index'])->name("get_transactions");
-Route::post('/submitEntry', [TransactionController::class, 'store'])->name('postTransact');
-
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\TransactionController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('login');
-});
-
-Route::middleware(['auth'])->group(function () {
-  Route::get('/home', [HomeController::class, 'index'])->name('home');
-  Route::get('/transactions', [TransactionController::class, 'index']);
-  Route::post('/submitEntry', [TransactionController::class, 'store'])->name('postTransact');
-
+    Route::get('/transactions', [TransactionController::class, 'index'])->name("get_transactions");
+    Route::post('/submitEntry', [TransactionController::class, 'store'])->name('postTransact');
 });
 
 Route::post('/dashboard', [LoginController::class, 'login'])->name('login');
